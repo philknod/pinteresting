0c439110856d60b3add1c477fcc3e5eade9f8a37
Philipp Knodelgit config --global user.name Philipp <philknodel@gmail.com>
2014-02-20 21:09:38 +0100

    add00test

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Pinteresting::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  # You can 00 have the root of your site routed with "root"
   # root 'welcome#index'
 
   # Example of regular route:
